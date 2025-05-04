e6d375c623072f77c61ab65a1db9e7c6c7162edd
Freddy <perezf.a24@gmail.com>
2025-05-04 15:10:48 -0600

    API Actualizada

--- api/src/main/java/karibeCar/api/repository/ReservaRepository.java
+++ api/src/main/java/karibeCar/api/repository/ReservaRepository.java
@@ -1,5 +1,7 @@
 package karibeCar.api.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import karibeCar.api.entity.Reserva;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Integer>{
+    boolean existsByIdAlquiler_IdVehiculo_IdVehiculoAndIdAlquiler_FechaInicioLessThanAndIdAlquiler_FechaFinGreaterThan(
+    int idVehiculo, LocalDateTime fechaFin, LocalDateTime fechaInicio
+);
 }
